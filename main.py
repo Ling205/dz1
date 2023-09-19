e6f8c9f5006a8c399a638c476bf622969fb155a5
Ling205 <leomord20@mail.ru>
2023-09-19 15:50:44 +0300

    Доработка (построение графика)

--- main.py
+++ main.py
@@ -5,4 +5,7 @@ plt.xlabel('Кол-во TOYOTA (X1)')
 plt.ylabel('Кол-во AUDI (X2)')
 plt.xlim(0,37)
 plt.ylim(0,40)
-#
+plt.plot([0, 28.2], [35.25, 0])
+plt.plot([0, 25], [19, 19])
+plt.plot([17, 17], [0, 25])
+plt.plot([0, 5], [22/5, 0])
