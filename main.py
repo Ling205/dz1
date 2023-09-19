8b59c77df2117eb9a4863390a79bbceb543783a2
Ling205 <leomord20@mail.ru>
2023-09-19 16:13:59 +0300

    Эксперимент 2 (изменение расположение графика 1)

--- main.py
+++ main.py
@@ -4,9 +4,9 @@ import matplotlib.pyplot as plt
 plt.title('График')
 plt.xlabel('Кол-во TOYOTA (X1)')
 plt.ylabel('Кол-во AUDI (X2)')
-plt.xlim(0,50)
+plt.xlim(0,70)
 plt.ylim(0,50)
-plt.plot([0, 28.2], [35.25, 0])
+plt.plot([0, 60], [40, 0]) #график 1
 plt.plot([0, 25], [19, 19])
 plt.plot([17, 17], [0, 25])
 plt.plot([0, 5], [22/5, 0])
